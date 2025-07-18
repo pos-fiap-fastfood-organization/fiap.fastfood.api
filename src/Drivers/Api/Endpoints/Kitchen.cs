01959cbc0d19210e841b88ad18ae3205ef713baa
Victor Montenegro <jvcmontenegro67@gmail.com>
2025-07-18 15:26:19 -0300

    add kitchen

---
+++ src/Drivers/Api/Endpoints/Kitchen.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Endpoints;
+
+[ApiController]
+[Route("[controller]")]
+public class Kitchen : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Kitchen");
+    }
+}
