729aa5e964718257dbce8aa1163342fe0d8e150d
Victor Montenegro <jvcmontenegro67@gmail.com>
2025-09-20 14:44:41 -0300

    add unit tests

---
+++ tests/UnitTests/Core.Tests/UseCaseTests/CustomerUseCaseTests/CustomerUseCaseTest.cs
@@ -0,0 +1,18 @@
+﻿using Core.Gateways.Interfaces;
+using Core.UseCases;
+using NSubstitute;
+
+namespace Core.Tests.UseCaseTests.CustomerUseCaseTests;
+
+public class CustomerUseCaseTest
+{
+    public readonly ICustomerGateway _customerGateway;
+    public readonly CustomerUseCase _sut;
+
+    public CustomerUseCaseTest()
+    {
+        _customerGateway = Substitute.For<ICustomerGateway>();
+
+        _sut = new CustomerUseCase(_customerGateway);
+    }
+}
